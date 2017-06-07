4ee599606d2fd37649c1c0c21958a1e1837d28a1
RajKhaitan <rajvardhan.khaitan@gmail.com>
2017-06-07 11:51:11 -0700

    Add files via upload

---
+++ Shower Buddy/Shower Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shower Buddy
+//
+//  Created by RAJ KHAITAN on 5/22/17.
+//  Copyright © 2017 entrepreneurship. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
